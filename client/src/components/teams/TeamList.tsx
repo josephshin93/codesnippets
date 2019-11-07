4641ab077901e90a0f67aa7e79cc9648b26e8234
tibbsm <tibbs.marc@gmail.com>
2019-11-07 22:19:32 +0900

    displaying team info

--- client/src/components/teams/TeamList.tsx
+++ client/src/components/teams/TeamList.tsx
@@ -7,23 +7,36 @@ interface Props {
     teams: any
 }
 
+const Team = ({ team, members }: any ) => {
+    console.log( "TEAM:", team, members);
+    return (
+      <div>
+        {Object.keys(members).map((member: any, index: any) => {
+          console.log(team.roles[member], member, index);  
+          return (
+            <div key={index}>{team.members[member]} - {team.roles[member]}</div>
+          )
+        })}
+      </div>
+    )
+}
+
 class TeamList extends Component<Props> {
     componentDidMount() {
-        this.props.fetchTeams();
-            // TODO FIX THIS...
-        setTimeout(() => {
-            console.log(this.props.teams);
-        }, 1000);
-        
+        this.props.fetchTeams();        
     }
 
     renderTeams() {
         return this.props.teams.map( (team: any, i: number) => {
+            console.log( i, team, team.members);
             return (
                 <div key= { i }>
-                    <h3>Team {i}:</h3>
-                    <p><b>Members/Roles:</b></p>
-           
+                    <h3>Team: { team.name.toUpperCase() }</h3>
+                    <h4>Members:</h4>
+                    <Team key={i} team={team} members={team.members}/>
+                    <h4>Subscriptions:</h4>
+                    <p>Digest: { team.subscription.digest.day } @ { team.subscription.digest.time }</p>
+                    <p>Digest: { team.subscription.reminder.day } @ { team.subscription.reminder.time }</p>
                 </div>
             )
         })
