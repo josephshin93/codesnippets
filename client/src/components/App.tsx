605db0d247e15d287fb08e4cb9a94596834cda01
wanahmaw <wanahmaw@oregonstate.edu>
2019-11-13 00:04:30 -0800

    Fixes

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from  'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-import Header from './Header';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import NewSnippet from './snippets/NewSnippet';
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import NewSnippet from "./snippets/NewSnippet";
 
 interface Props {
-    fetchUser: () => void;
+  fetchUser: () => void;
 }
 
 class App extends Component<Props> {
-    componentDidMount() {
-        this.props.fetchUser();
-    }
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Header/>
-                        <Route exact path="/" component={Landing} />  
-                        <Route exact path="/dashboard" component={Dashboard} />  
-                        <Route exact path="/newsnippet" component={NewSnippet} />  
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/newsnippet" component={NewSnippet} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default connect(null, actions)(App);
+export default connect(
+  null,
+  actions
+)(App);
