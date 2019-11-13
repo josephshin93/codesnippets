95bbefec28519c9c0e0edf1d2c7e9c1b11a94805
wanahmaw <wanahmaw@oregonstate.edu>
2019-11-13 00:01:23 -0800

    Form with Formik & Yup

--- client/src/components/snippets/NewSnippet.tsx
+++ client/src/components/snippets/NewSnippet.tsx
@@ -1,79 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
+import AddSnippetForm from "./AddSnippetForm";
 
 interface Props {
+  // TODO: Typescript format
   addSnippet: (values: any) => void;
   history: any;
   auth: any;
+  teams: any;
 }
 
 class NewSnippet extends Component<Props> {
-  handleSubmit = (event: any) => {
-    event.preventDefault();
-    // console.log(event.target[0].value);
-    this.props.addSnippet({
-      title: event.target[0].value,
-      content: event.target[1].value,
-      description: event.target[2].value,
-      status: event.target[3].value,
-      team: event.target[4].value,
-      ownerID: this.props.auth.googleId,
-      ownerName: this.props.auth.firstName + " " + this.props.auth.lastName
-    });
-
-    // TODO FIX THIS...
-    setTimeout(() => {
-      this.props.history.push("/dashboard");
-    }, 500);
-  };
-
-  // TODO FIX THIS...
-  handleClick = () => {
-    this.props.history.push("/dashboard");
-  };
-
+  // Create options for Team select dropdown
+  createListItems() {
+    if (this.props.auth) {
+      let teams = this.props.auth.teams;
+      return Object.keys(teams).map(k => {
+        return (
+          <option key={k} value={teams[k]}>
+            {teams[k]}
+          </option>
+        );
+      });
+    } else return null;
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
-            <div className="col s12">
-              <h1>New Snippet</h1>
-              <form onSubmit={this.handleSubmit}>
-                Title:
-                <br />
-                <input type="text" name="title"></input>
-                <br />
-                Content:
-                <br />
-                <input type="text" name="content"></input>
-                <br />
-                Description:
-                <br />
-                <input type="text" name="description"></input>
-                <br />
-                Status:
-                <br />
-                <input type="text" name="status"></input>
-                <br />
-                Team:
-                <br />
-                <input type="text" name="team"></input>
-                <br />
-                <br />
-                <button className="btn waves-effect waves-light red" 
-                  onClick={this.handleClick}
-                  style={{marginRight:"10px"}}>
-                  Cancel
-                  <i className="material-icons right">cancel</i>
-                </button>
-                <button className="btn waves-effect waves-light blue">
-                  Submit
-                  <i className="material-icons right">send</i>
-                </button>
-              </form>
-            </div>
-        </div>    
+          <div className="col s12">
+            <h3>New Snippet</h3>
+            {
+              <AddSnippetForm
+                teams={this.createListItems()}
+                addSnippet={this.props.addSnippet}
+                auth={this.props.auth}
+                history={this.props.history}
+              />
+            }
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,17 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import reduxThunk from 'redux-thunk';
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import reduxThunk from "redux-thunk";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-    <Provider store={store}><App /></Provider>,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
