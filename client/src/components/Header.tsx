0c7212d6e2126faf362d83f85b378209b2ec3be2
wanahmaw <wanahmaw@oregonstate.edu>
2019-11-13 23:39:25 -0800

    Removed errors after move from auth prop to user

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,62 +1,68 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import {
-    State,
-    User,
-} from '../store/types';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { State, User } from "../store/types";
 
 interface HeaderProps {
-    user: User | null;
+  user: User | null;
 }
 
 class Header extends Component<HeaderProps> {
-
-    renderContent() {
-        switch (this.props.user) {
-            // case null:
-            //     return 'Loading...';
-            case null:
-                return <div><a href="auth/google">Log In</a></div>;
-            default:
-                return [
-                    
-                    <li key="1">
-                        <img src={this.props.auth.picture} alt="avatar" 
-                            className="circle" width="25" height="25"
-                            style={{
-                                verticalAlign: "middle", 
-                                marginBottom: "4px", 
-                                marginRight: "10px"}}>
-                        </img>
-                        {this.props.auth.firstName}
-                    </li>,
-                    <li key="2"><a style={{fontWeight: "bold"}} href="api/logout">Log Out</a></li>
-                ];
-        }
-    }
-
-    render () {
+  renderContent() {
+    switch (this.props.user) {
+      // case null:
+      //     return 'Loading...';
+      case null:
         return (
-            <nav>
-                <div className="nav-wrapper blue">
-                
-                    <Link to="/dashboard" className="left brand-logo">
-                        <i className="material-icons" style={{marginLeft: "15px"}}>bubble_chart</i>
-                        Boba
-                    </Link>
-                    <ul className="right">
-                        {this.renderContent()} 
-                    </ul>                    
-                </div>
-            </nav>
+          <div>
+            <a href="auth/google">Log In</a>
+          </div>
         );
+      default:
+        return [
+          <li key="1">
+            <img
+              src={this.props.user.picture}
+              alt="avatar"
+              className="circle"
+              width="25"
+              height="25"
+              style={{
+                verticalAlign: "middle",
+                marginBottom: "4px",
+                marginRight: "10px"
+              }}
+            ></img>
+            {this.props.user.firstName}
+          </li>,
+          <li key="2">
+            <a style={{ fontWeight: "bold" }} href="api/logout">
+              Log Out
+            </a>
+          </li>
+        ];
     }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper blue">
+          <Link to="/dashboard" className="left brand-logo">
+            <i className="material-icons" style={{ marginLeft: "15px" }}>
+              bubble_chart
+            </i>
+            Boba
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 const mapStateToProps = (state: State) => {
-    return { user: state.user };
-}
+  return { user: state.user };
+};
 
 export default connect(mapStateToProps)(Header);
