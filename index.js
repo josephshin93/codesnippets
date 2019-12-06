4fc1efc3efd95c74a9c1a722a34556f915452488
tibbsm <tibbs.marc@gmail.com>
2019-12-06 11:52:08 +0900

    fixing uncompiled index.ts => .js file

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var express = require("express");
 var keys = require("./config/keys");
 var cookieSession = require("cookie-session");
 var app = express();
-var bodyParser = require("body-parser");
-var passport = require("passport");
-var admin = require("firebase-admin");
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var admin = require('firebase-admin');
+var scheduler = require('./services/scheduler');
 app.use(bodyParser.json());
 app.use(cookieSession({
     maxAge: 15 * 24 * 60 * 60 * 1000,
@@ -32,30 +33,46 @@ require("./services/passport")(firebase);
 app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app, firebase);
+require('./routes/usersRoutes')(app, firebase);
 require('./routes/snippetRoutes')(app, firebase);
 require('./routes/teamRoutes')(app, firebase);
-// Testing emailer/scheduler
-var fakeMember = { "FAKE USER ID": "FAKE USER NAME" };
-var fakeRole = { "FAKE USER ID": "admin" };
-var fakeSubs = [{
-        title: "FAKE SUB TITLE",
-        issueTime: 18,
-        issueDay: 6,
-        content: "FAKE SUB CONTENT"
-    }];
-var team = {
-    name: "FAKE TEAM",
-    members: fakeMember,
-    roles: fakeRole,
-    subscriptions: fakeSubs
-};
-var sub = "sub";
-require('./services/scheduler')(firebase, team);
+// TODO
+// FOR TESTING EMAIL SCHEDULER REMOVE WHEN FINISHED
+// var date = new Date;
+// var hours = date.getHours();
+// var day = date.getDay();
+// // Testing emailer/scheduler
+// var fakeMember = { "aKJtboKgff9o0CyTzJbY": "Marc Tibbs", "R8vDws3j8PYBzojx3Vzf": "Marc Christopher Tibbs"};
+// var fakeRole = { "aKJtboKgff9o0CyTzJbY": "admin", "R8vDws3j8PYBzojx3Vzf": "member" } ;
+// var fakeSubs = [
+// { 
+//   title: "FAKE DIGEST",
+//   issueTime: hours,  // 0-23
+//   issueDay: day,    // 0-7 (Sunday = 0 OR 7)
+//   content: "FAKE SUB CONTENT",
+//   type: "digest" // "digest" || "reminder" 
+// }, 
+// { 
+//   title: "FAKE REMINDER",
+//   issueTime: hours,  // 0-23
+//   issueDay: day,    // 0-7 (Sunday = 0 OR 7)
+//   content: "FAKE SUB CONTENT",
+//   type: "reminder" // "digest" || "reminder" 
+// }
+// ];
+// var team =  {
+//   name: "blue",
+//   members: fakeMember,
+//   roles: fakeRole,
+//   subscriptions: fakeSubs
+// }
+// scheduler.scheduleSubscriptions(firebase, team);
+scheduler.scheduleAllOnStart(firebase);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    var path_1 = require('path');
-    app.get('*', function (req, res) {
-        res.sendFile(path_1.resolve(__dirname, 'client', 'build', 'index.html'));
+    var path_1 = require("path");
+    app.get("*", function (req, res) {
+        res.sendFile(path_1.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 var PORT = process.env.PORT || 5000;
